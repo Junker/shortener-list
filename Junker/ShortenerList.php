8846eed6a87c0a6d8f7be3ccf6311843d8d1fe38
Junker <dk-junker@ya.ru>
2017-10-29 22:15:03 +0700

    main commit

---
+++ Junker/ShortenerList.php
@@ -0,0 +1,14 @@
+<?php
+namespace Junker;
+
+class ShortenerList
+{
+	const FILE_PATH = __DIR__ . '/shorturl-services-list.txt';
+
+	public static function getAll()
+	{
+		$file = file(FILE_PATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+		
+		return $file; 
+	}
+}
